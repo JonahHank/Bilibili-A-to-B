776f5df1c3a5b54c32cb8d6bb1b3d0ccacb6efc9
Jonah Hank <62379610+JonahHank@users.noreply.github.com>
2020-03-25 17:28:31 +0800

    Source file.

---
+++ AtoB/MainForm.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "输入原始AV号"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(300, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "输出新的BV号"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 73)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(300, 21)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(229, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 27)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "确定(&R)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.Enabled = False
+        Me.Label3.ForeColor = System.Drawing.Color.Gray
+        Me.Label3.Location = New System.Drawing.Point(12, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(300, 28)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "输入AV号时支持""Ctrl + V""快捷键，输出的BV号目前仅支持右键复制"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 162)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "A TO B"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ AtoB/MainForm.vb
@@ -0,0 +1,73 @@
+﻿Option Explicit On
+Public Class MainForm
+    Const TB = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
+    Const NumX = 177451812
+    Const NumA = 100618342136696320
+
+    'Dim BThread As Threading.Thread
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'BThread = New Threading.Thread(AddressOf AV2BV)
+    End Sub
+
+    Public Function AV2BV(A As Long) As String
+        Dim n， x As Long, i As Integer
+        Dim t() As String, f(9) As String
+        AV2BV = "0000000000"
+        Try
+            x = (A Xor NumX) + NumA '异或运算
+            For i = 0 To 9
+                n = x \ (58 ^ i) Mod 58
+                f(i) = n
+            Next
+            t = SplitString(TB)
+            For i = 0 To 9
+                f(i) = Replace(f(i), f(i), t(Val(f(i)) + 1))
+            Next
+            '排序
+            AV2BV = f(6) & f(2) & f(4) & f(8) & f(5) & f(9) & f(3) & f(7) & f(1) & f(0)
+        Catch e As Exception
+            MessageBox.Show(e.Message)
+        End Try
+    End Function
+
+    Public Function D2B(Num As Long) As String '二进制转换(未使用)
+        D2B = ""
+        Do While Num > 0
+            D2B = Num Mod 2 & D2B
+            Num \= 2
+        Loop
+    End Function
+
+    Public Function SplitString(Text As String) As String() '切割字符串
+        Dim i As Integer, s(58) As String
+        For i = 1 To 58
+            s(i) = Mid(Text, i, 1)
+        Next
+        SplitString = s
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If TextBox1.Text = "" Then Exit Sub '不能为空
+            If InStr(LCase(TextBox1.Text), "av", CompareMethod.Text) Then
+                TextBox2.Text = "BV" & AV2BV(Strings.Right(TextBox1.Text, Len(TextBox1.Text) - 2))
+            Else
+                TextBox2.Text = "BV" & AV2BV((TextBox1.Text))
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        '输出文本框不能被用户编辑
+        If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub MainForm_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        End
+    End Sub
+End Class
